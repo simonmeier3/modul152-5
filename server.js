4b57f0abc06023c3b7b52256417c3f4794731f27
Simon Meier <meier-simon@gmx.ch>
2021-04-13 20:24:09 +0200

    Initial Commit

---
+++ server.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var WebSocket = require("ws");
+var wss = new WebSocket.Server({
+    port: 8080,
+});
+wss.on('connection', function (client) {
+    client.on('message', function (data) {
+        Array.from(wss.clients)
+            .filter(function (connectedClient) { return connectedClient !== client; })
+            .forEach(function (connectedClient) { return connectedClient.send(data); });
+    });
+    client.send('Herzlich Willkommen!');
+});
+//# sourceMappingURL=server.js.map
---
+++ server.ts
@@ -0,0 +1,16 @@
+import * as WebSocket from "ws";
+
+const wss = new WebSocket.Server({
+    port: 8080,
+});
+
+wss.on('connection', (client: WebSocket) => {
+
+    client.on('message', data => {
+       Array.from(wss.clients)
+           .filter(connectedClient => connectedClient !== client)
+           .forEach(connectedClient => connectedClient.send(data));
+    });
+    client.send('Herzlich Willkommen!')
+
+});
